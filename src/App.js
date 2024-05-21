c93c3d38e6a81b48c807714b22492a4ef51e2172
KipkorirNovestus <kipkorirnovestus1@gmail.com>
2024-05-21 09:04:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Students from './Components/Students';
+
+function App() {
+  return (
+    
+    <>
+    <div class="container">
+      <Students/>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
